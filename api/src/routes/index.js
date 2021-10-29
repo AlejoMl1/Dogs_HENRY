67c9e3b983fb217e5c15cae39ce572b0976e3ac9
AlejoMl1 <a.munoz254@uniandes.edu.co>
2021-10-29 13:01:06 -0500

    api right place

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,5 +2,16 @@ const { Router } = require('express');
 
 const router = Router();
 
+//import the differents routes
+const dogRoutes = require('./dog')
+const dogsRoutes = require('./dogs')
+const temperamentRoutes = require('./temperament')
+
+const router = Router();
+//told express to use especific routes 
+router.use('/dog', dogRoutes)
+router.use('/dogs', dogsRoutes)
+router.use('/temperamentRoutes', temperamentRoutes)
+
 
 module.exports = router;
